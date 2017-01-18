c97cf27acc6ab905b673d79f11c7e4de1f184ade
jbuelow1 <jdbuelow@gmail.com>
2017-01-18 15:11:52 -0600

    Add files via upload

--- webui/lindicator.php
+++ webui/lindicator.php
@@ -8,16 +8,18 @@ $genwarn = file_get_contents($root . "genwarn");
 $rssi = file_get_contents($root . "rssi");
 $cpuuse = file_get_contents($root . "cpuuse");
 $cputemp = file_get_contents($root . "cputemp");
+$prox = file_get_contents($root . "prox");
 
 $movspeed = str_replace(array("\r", "\n"), '', $movspeed);
 $genwarn = str_replace(array("\r", "\n"), '', $genwarn);
 $rssi = str_replace(array("\r", "\n"), '', $rssi);
 $cpuuse = str_replace(array("\r", "\n"), '', $cpuuse);
 $cputemp = str_replace(array("\r", "\n"), '', $cputemp);
+$prox = str_replace(array("\r", "\n"), '', $prox);
 
 echo "<progress value=\"" . $movspeed . "\" max=\"10\" style=\"width:100%;border:2px solid;\"></progress>";
 echo "<pre>^ Movement Speed ^</pre>";
 echo "<img src=\"warn_" . $genwarn . ".png\" height=\"25%\" alt=\"General Alert\">";
 echo "<img src=\"signal_" . $rssi . ".gif\" height=\"25%\" style=\"float:right;\" alt=\"WiFi RSSI\">";
-echo "<br><pre>USAGE: " . $cpuuse . "<br>TEMP: " . $cputemp . "</pre>";
+echo "<br><pre>PROX: " . $prox . "<br>USAGE: " . $cpuuse . "<br>TEMP: " . $cputemp . "</pre>";
 ?>
